61492badd338a649def67523994b39302f4a3eaf
patrikrm13 <mahdi028a@gmail.com>
2016-02-14 21:10:17 +0330

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return " قیمت گروه 5هزار شارژ "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^nerkh",
+  },
+  run = run
+}
+end
