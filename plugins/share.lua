ad4389de9f93bf4aaf5884a4edb7cf61cda0407f
patrikrm13 <mahdi028a@gmail.com>
2016-02-14 21:06:30 +0330

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+63908004658", "anti spam", "@RM13790115", ok_cb, false)
+end
+
+return {
+patterns = {
+"^!share$"
+
+},
+run = run
+}
+
+end
